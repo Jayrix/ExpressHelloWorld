608d341f9174fc449ed23a8ca20a0ee4daf01457
Jakub Kozlik <kozlik.jakub@gmail.com>
2018-01-23 12:27:33 +0100

    added MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
+
+
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
 app.get('/', (req,res) => {
-    res.send('Hello world!');
+    res.sendFile('/home/kuba/wlasne/ExpressHelloWorld/index.html');
+    db.collection('quotes').find().toArray((err,results) => {
+        console.log(results);
+    })
 });
 
-console.log('will respond with Hello string');
+app.post('/quotes', (req,res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err);
 
-app.listen('3002', ()=> {
-    console.log('express server listening on port 3002');
+        console.log('saved to database');
+        res.redirect('/');
+    })
 })
+
+
+let db;
+
+MongoClient.connect('mongodb://JK:passwdJK1@ds113098.mlab.com:13098/tutorial_db', (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('tutorial_db');
+    app.listen('3002', ()=> {
+        console.log('express server listening on port 3002');
+    })
+})
+
